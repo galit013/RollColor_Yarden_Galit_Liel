80475e88bc9619f3ed1eb508e941ee032cbe2535
Galit13 <14459@meir.ort.org.il>
2022-03-31 20:16:49 +0300

    move block

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import pygame
 from constants import *
 from App import *
 from Player import *
-from Block import*
-from Button import*
+from Block import *
+import time
 
 
 def main():
+    score = 0
     # Set up the game display, clock and headline
     pygame.init()
     # Create the screen and show it
@@ -15,14 +16,6 @@ def main():
 
     # Change the title of the window
     pygame.display.set_caption('RollColor')
-
-    img = pygame.image.load(QUESTION_MARK)
-    img = pygame.transform.scale(img, (QUESTION_MARK_WIDTH,QUESTION_MARK_HEIGHT))
-    screen.blit(img, (QUESTION_MARK_X_POS,QUESTION_MARK_Y_POS))
-
-    question_mark=Button(QUESTION_MARK_X_POS,QUESTION_MARK_Y_POS,QUESTION_MARK_HEIGHT,QUESTION_MARK_WIDTH)
-
-
 
     ball = Player(screen)
     global display_screen
@@ -37,7 +30,6 @@ def main():
     # Display all drawings we have defined
     pygame.display.flip()
 
-
     running = True
     loaded = False
     while running:
@@ -48,10 +40,11 @@ def main():
                 running = False
             # checks if the user pressed the mouse button
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_click_pos = event.pos
-                if question_mark.mouse_in_button(mouse_click_pos):
-                    print(6)
                 print(1)
+                block = Block(screen, RED, 420, 340)
+                display_screen.game_screen(screen, block.get_color(), block.get_x_pos(), block.get_y_pos())
+                pygame.display.flip()
+
                 loaded = True
 
             elif loaded:
@@ -59,13 +52,20 @@ def main():
                 if key[pygame.K_LEFT]:
                     print(2)
                     ball.move_player_left(screen)
+                    block.move_block(screen)
+
                 if key[pygame.K_RIGHT]:
                     print(3)
                     ball.move_player_right(screen)
-
-
-
-
+                # Updating the score based on the keys events
+                while key[pygame.K_RIGHT] or key[pygame.K_LEFT]:
+                    score += 10
+                    time.sleep(0.2)
+                    print(score)
+                    game_over = True
+                    break
+                # Saving the last score as the best
+                best_score = score
 
         # Set the clock tick to be 60 times per second. 60 frames for second.
         # If we want faster game - increase the parameter.
